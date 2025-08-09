6269402e4b7e84ef01d508c1c0e038abf0d723c5
kimsniper <kim1991mail@gmail.com>
2025-08-09 13:12:18 +0800

    added servo functionaity

--- main/main.cpp
+++ main/main.cpp
@@ -29,72 +29,72 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-// #include <iostream>
-// #include <thread>
-// #include <chrono>
-// #include <memory>
-// #include <string>
-// #include <sstream>
-// #include <esp_pthread.h>
-// #include <freertos/FreeRTOS.h>
-// #include <freertos/task.h>
-// #include <esp_log.h>
-
-// #include "servo.hpp"
-
-// using namespace std::chrono_literals;
-
-// static const char* TAG = "main";
-
-// static Servo servo;
-
-// void servo_control_thread()
-// {
-//     if (servo.attach(0) != Pwm_Error_t::PWM_OK) {
-//         std::cerr << "Failed to attach servo" << std::endl;
-//         return;
-//     }
-
-//     const int angles[] = {0, 90, 180, 90};
-//     size_t index = 0;
-
-//     while (true) {
-//         int angle = angles[index];
-//         std::cout << "Servo angle: " << angle << " deg" << std::endl;
-//         servo.setAngle(angle);
-
-//         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-//         index = (index + 1) % (sizeof(angles) / sizeof(angles[0]));
-//     }
-// }
-
-// extern "C" void app_main(void)
-// {
-//     // Servo thread
-//     esp_pthread_cfg_t cfg_servo = esp_pthread_get_default_config();
-//     cfg_servo.stack_size = 4096;
-//     cfg_servo.prio = 5;
-//     cfg_servo.pin_to_core = 1;  // or 0 / 1 to pin to a specific core tskNO_AFFINITY -- any core
-//     cfg_servo.thread_name = "servo_thread";
-
-//     ESP_ERROR_CHECK(esp_pthread_set_cfg(&cfg_servo));
-
-//     // Launch the C++ threads
-//     std::thread servo_thread(servo_control_thread);
-//     servo_thread.detach();  // Let it run independently
-// }
-
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <esp_pthread.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_log.h>
 #include "who_camera.h"
 #include "who_human_face_detection.hpp"
 
+#include "servo.hpp"
+
 static QueueHandle_t xQueueAIFrame = NULL;
 
-extern "C" void app_main()
+// using namespace std::chrono_literals;
+
+static const char* TAG = "main";
+
+static Servo servo1, servo2;
+
+void servo_control_thread()
+{
+    if (servo1.attach(12) != Pwm_Error_t::PWM_OK) {
+        std::cerr << "Failed to attach servo" << std::endl;
+        return;
+    }
+
+    if (servo2.attach(13) != Pwm_Error_t::PWM_OK) {
+        std::cerr << "Failed to attach servo" << std::endl;
+        return;
+    }
+
+    const int angles[] = {0, 90, 180, 90};
+    size_t index = 0;
+
+    while (true) {
+        int angle = angles[index];
+        std::cout << "Servo angle: " << angle << " deg" << std::endl;
+        servo1.setAngle(angle);
+        servo2.setAngle(angle);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+        index = (index + 1) % (sizeof(angles) / sizeof(angles[0]));
+    }
+}
+
+extern "C" void app_main(void)
 {
     xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
 
     register_camera(PIXFORMAT_RGB565, FRAMESIZE_QVGA, 2, xQueueAIFrame);
     register_human_face_detection(xQueueAIFrame, NULL, NULL, NULL, true);
-}
 
+    // Servo thread
+    esp_pthread_cfg_t cfg_servo = esp_pthread_get_default_config();
+    cfg_servo.stack_size = 4096;
+    cfg_servo.prio = 6;
+    cfg_servo.pin_to_core = 0;
+    cfg_servo.thread_name = "servo_thread";
+
+    ESP_ERROR_CHECK(esp_pthread_set_cfg(&cfg_servo));
+
+    std::thread servo_thread(servo_control_thread);
+    servo_thread.detach();
+}
